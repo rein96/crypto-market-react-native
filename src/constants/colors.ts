9913d02a35527e96b5faa03003aad1496b0a70ec
Reinhart Andreas <reinhartandreas96@gmail.com>
2022-09-03 23:57:42 +0700

    feat: add colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,28 @@
+export const colors = {
+  brand: {
+    primary: '#2182BD',
+    secondary: '#5282BD',
+    muted: '#C6DAF7',
+  },
+  ui: {
+    primary: '#eeeeee',
+    secondary: '#757575',
+    tertiary: '#F1F1F1',
+    quaternary: '#FFFFFF',
+    disabled: '#DEDEDE',
+    error: '#D0421B',
+    success: '#138000',
+  },
+  bg: {
+    primary: '#FFFFFF',
+    secondary: '#F1F1F1',
+  },
+  text: {
+    primary: '#171717',
+    secondary: '#979797',
+    disabled: '#9C9C9C',
+    inverse: '#FFFFFF',
+    error: '#ff5a5a',
+    success: '#1ccb22',
+  },
+};
