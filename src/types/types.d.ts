cb3d60388f151b778b3c755d7bacee9a0163eb94
Reinhart Andreas <reinhartandreas96@gmail.com>
2022-09-03 18:55:43 +0700

    feat: setup interfaces

---
+++ src/types/types.d.ts
@@ -0,0 +1,51 @@
+export interface PriceDataInterface {
+  pair: string;
+  latestPrice: string;
+  day: null | string;
+  week: null | string;
+  month: null | string;
+  year: null | string;
+}
+
+export interface CryptocurrencyInterface {
+  currencyGroup: string;
+  color: string;
+  currencySymbol: string;
+  name: string;
+  logo: string;
+  decimal_point: number;
+  listingDate: Date;
+  wallets: WalletInterface[];
+}
+
+export interface SortedPricePairInterface {
+  [key: string]: PriceDataInterface;
+}
+
+interface WalletInterface {
+  currencyGroup: string;
+  tokenSymbol: string;
+  decimal_point: number;
+  tokenType: string;
+  blockchain: string;
+  explorer: string;
+  listingDate: Date;
+  blockchainName: string;
+  logo: string;
+}
+
+/**
+ * T is required generic
+ * U is optional generic
+ * */
+export interface ResponseInterface<T, U = void> {
+  code: string;
+  message: string;
+  payload: T;
+  [key: string]: U;
+}
+
+export interface PriceChangesResponseInterface
+  extends ResponseInterface<PriceDataInterface[]> {
+  sortedPricePairData: SortedPricePairInterface;
+}
